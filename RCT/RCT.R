8604bdadc5916db33062e5a6a15b6724220488d4
Sean Carmody <seancarmody@gmail.com>
2010-10-25 10:32:52 +1100

    Added some extra comments

--- RCT/RCT.R
+++ RCT/RCT.R
@@ -16,11 +16,14 @@ rct <- function(cases, type="square", border=par("fg"),
 	if (!(type %in% names(rct.plans))) type <- "square"
 	rct <- rct.plans[[type]]
 	
+	# Get floor plan
 	plan <- rct$plan
 	n <- dim(plan)[1]
 	if (length(fill) < length(cases)) fill <- rep(fill, length(cases))[1:length(cases)]
 
+	# Assign seats
 	plan$taken <- FALSE
+	# Set random seed
 	if (!is.null(seed)) set.seed(seed)
 	seats <- sample(1:n, sum(cases))
 	plan$taken[seats] <- TRUE
@@ -28,6 +31,7 @@ rct <- function(cases, type="square", border=par("fg"),
 	# Default shading: shades of grey
 	if (is.null(fill)) fill <- gray(0:(length(cases)-1)/length(cases))
 
+	# Colour each case type differently
 	col.hi <- cumsum(cases)
 	col.lo <- c(1, col.hi[-length(col.hi)]+1)
 	plan$col <- NA
@@ -35,20 +39,26 @@ rct <- function(cases, type="square", border=par("fg"),
 		plan$col[seats[col.lo[i]:col.hi[i]]] <- fill[i]
 	}
 	
+	# Get text labels (if they exist)
 	l <- rct$labels	
 	if (is.null(l)) label <- FALSE
 	
+	# Format label: "x cases in n"
 	if (is.null(xlab)) xlab <- paste(prettyNum(sum(cases), big.mark=","),
 		"cases in", prettyNum(n, big.mark=","))
 	
+	# Plot RCT
 	if (draw.plot) {
 		plot(c(0, 1), c(0, 1), type="n", axes=FALSE, xlab=xlab, ylab=ylab)
 		rect(plan$xleft, plan$ybottom, plan$xright, plan$ytop,
 			border=border, col=plan$col)
+		# Add text labels (if required)
 		if (label) {
 			if (is.na(l$pos[1])) l$pos <- NULL
 			text(l$x, l$y, l$text, pos=l$pos, col=lab.col, xpd=NA, cex=lab.cex)
 			}
+		# Return the plan data (including taken seats and colours)
+		# if the chart is not plotted
 		} else return(plan)
 	}	
 	
